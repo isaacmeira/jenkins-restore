41ec8e87f251c937894dd878f1ed207abe641f72
Isaac Tadeu Meira <CIANDT\isaacm@lnb024495bhz.ciandt.global>
2021-04-16 11:46:47 -0300

    init

---
+++ restore.sh
@@ -0,0 +1,11 @@
+readonly BACKUP_FILE="$1"
+readonly JENKINS_PATH = "/var/jenkins_home/"
+
+# Usage: ./jenkins-backup.sh backup_YYYmmDD  Ex: backupt_20210416
+
+cd /var/jenkins_home/backups_pipe/
+tar xzvf ${BACKUP_FILE}.tar.gz 
+cp -R jenkins-backup/* ${JENKINS_PATH}
+chown jenkins:jenkins -R ${JENKINS_PATH}
+
+
