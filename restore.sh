e82ac469ba5c9ed8c2de7bb0ffa8de41c2433f39
Isaac Tadeu Meira <CIANDT\isaacm@lnb024495bhz.ciandt.global>
2021-04-16 11:53:46 -0300

    adding steps

--- restore.sh
+++ restore.sh
@@ -3,9 +3,13 @@ readonly JENKINS_PATH="/var/jenkins_home/"
 
 # Usage: ./jenkins-backup.sh backup_YYYmmDD  Ex: backupt_20210416
 
+echo "Entering Jenkins Folder"
 cd /var/jenkins_home/backups_pipe/
+echo "Unziping backup"
 tar xzvf ${BACKUP_FILE}.tar.gz 
+echo "Moving Backup"
 cp -R jenkins-backup/* ${JENKINS_PATH}
+echo "Changing owner"
 chown jenkins:jenkins -R ${JENKINS_PATH}
 
 
