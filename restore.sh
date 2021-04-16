7fdea0120f7b45134c9ce07f826709b71daca37e
Isaac Tadeu Meira <CIANDT\isaacm@lnb024495bhz.ciandt.global>
2021-04-16 11:58:27 -0300

    remove folder

--- restore.sh
+++ restore.sh
@@ -11,5 +11,6 @@ echo "Moving Backup to ${JENKINS_PATH}"
 cp -R jenkins-backup/* ${JENKINS_PATH}
 echo "Changing owner"
 chown jenkins:jenkins -R ${JENKINS_PATH}
-
+echo "Removing older backup folder"
+rm -rf /var/jenkins_home/backups_pipe/jenkins-backup
 
